f8d993e679dc93e18a487a9a18ba567a9ec5e079
dilumd <dilum.dar@gmail.com>
2016-05-01 08:56:19 +0530

    Added Class to load on requre

--- requirejs/scripts/helper/util.js
+++ requirejs/scripts/helper/util.js
@@ -4,16 +4,22 @@
 
 	$.myApp = function () {
 
-		
+		this.name = 'Test';
 
 		this.set = function (value) {
 
+			this.name = value;
+		};
+
+		this.get = function () {
+
+			return this.name;
 		};
 	};
 
 })(jQuery);
 
-$(document).ready (function () {
+//$(document).ready (function () {
 
 	var __myApp = new $.myApp ();
-});
+//});
