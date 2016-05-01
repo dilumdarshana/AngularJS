23688723d8b3946d5502b5f2c4445060ce866edb
dilumd <dilum.dar@gmail.com>
2016-05-01 08:26:09 +0530

    Test requirejs app

---
+++ requirejs/scripts/helper/util.js
@@ -0,0 +1,19 @@
+"use stricts";
+
+(function ($) {
+
+	$.myApp = function () {
+
+		
+
+		this.set = function (value) {
+
+		};
+	};
+
+})(jQuery);
+
+$(document).ready (function () {
+
+	var __myApp = new $.myApp ();
+});
